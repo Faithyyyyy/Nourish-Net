039160f074bf066e6e628eb698daa254b7b42544
faithyyyy <faithfortune6@gmail.com>
2023-05-22 21:57:05 +0100

    fetche the data

--- api/getFoodData.jsx
+++ api/getFoodData.jsx
@@ -2,15 +2,28 @@
 const getFoodData = async () => {
   console.log("api key", process.env.NEXT_PUBLIC_API_KEY);
   console.log("api id", process.env.NEXT_PUBLIC_API_ID);
+  // const res = await fetch(
+  //   `https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.NEXT_PUBLIC_API_ID}&app_key=${process.env.NEXT_PUBLIC_API_KEY}&nutrition-type=cooking&category=fast-foods`
+  // );
+  // if (!res.ok) {
+  //   console.log("kiloshele");
+  // }
+  // const response = res.json();
+  //   response.then((data) => console.log(data));
+  //   return res.json();
+  // https://www.themealdb.com/api/json/v1/1/search.php?f=a
   const res = await fetch(
-    `https://api.edamam.com/api/food-database/v2/parser?app_id=${process.env.NEXT_PUBLIC_API_ID}&app_key=${process.env.NEXT_PUBLIC_API_KEY}&nutrition-type=cooking&category=fast-foods`
+    `https://www.themealdb.com/api/json/v1/1/search.php?f=b`
   );
   if (!res.ok) {
     console.log("kiloshele");
   }
   const response = res.json();
-  //   response.then((data) => console.log(data));
-  //   return res.json();
+  const data = response.then((data) => {
+    console.log(data);
+  });
+  // return data;
+  console.log(data);
 };
 
 export default getFoodData;
--- components/AllDishes.jsx
+++ components/AllDishes.jsx
@@ -1,8 +1,29 @@
 import Image from "next/image";
+import getFoodData from "@/api/getFoodData";
+
 const AllDishes = () => {
+  const data = getFoodData();
+  console.log(data);
   return (
     <div className="mt-16">
-      <div className="w-full max-w-[200px] relative">
+      {/* {data.map((d) => {
+        return (
+          <div key={d.id} className="w-full max-w-[200px] relative">
+            <Image
+              src={d.strMealThumb}
+              alt={d.strMeal}
+              className="object-contain absolute -top-10 left-8"
+              width={136}
+              height={176}
+            />
+            <div className="w-full bg-darkBg2 pt-[114px] px-10 pb-[48px] text-sm text-center">
+              <p className="text-white  ">{d.strMeal}</p>
+              <p className="text-white mt-2 font-thin">${d.strMeasure3}</p>
+            </div>
+          </div>
+        );
+      })} */}
+      {/* <div className="w-full max-w-[200px] relative">
         <Image
           src="/assets/exampleFood.png"
           alt="Food Image"
@@ -14,7 +35,7 @@ const AllDishes = () => {
           <p className="text-white  ">Spicy seasoned seafood noodles</p>
           <p className="text-white mt-2 font-thin">$ 1.29</p>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
